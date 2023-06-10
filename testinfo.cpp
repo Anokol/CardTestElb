72921b2e9cc7e7d3c65d5bcb75e8ef6c011e4d3c
Anokol <136174524+Anokol@users.noreply.github.com>
2023-06-10 17:13:29 +0300

    Delete testinfo.cpp

--- testinfo.cpp
+++
@@ -1,9 +0,0 @@
-#include "testinfo.h"
-
-TestInfo::TestInfo(QString type, int fps, double frameTime, QString system)
-{
-    m_type = type; // названия теста текстурирование и тд
-    m_fps = fps;
-    m_frameTime = frameTime;
-    m_system = system; //тип системы
-}
